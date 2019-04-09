a024a51ccc1a785e0c4892aad4bcf22a630ade07
smarunich <marunich.s@gmail.com>
2019-04-09 04:10:02 +0000

    swap ctrl object to eip object to list public IPs

--- list_ctrl_ips.sh
+++ list_ctrl_ips.sh
@@ -3,7 +3,7 @@ let count=`grep student_count terraform.tfvars | awk '{print $3}'`
 idx=0
 for i in `seq $count`; do
   echo -n "Student$i: "
-  terraform state show aws_instance.ctrl[$idx] | grep 'public_ip ' | awk '{print $3}'
+  terraform state show aws_eip.ctrl_eip[$idx] | grep 'public_ip ' | awk '{print $3}'
   ((idx++))
 done
 let count=`grep server_count terraform.tfvars | awk '{print $3}'`
