198a25886e7833855324817deaf6522e2051a550
Sergey Marunich <marunich.s@gmail.com>
2019-06-17 10:05:45 +0300

    converting to terraform 0.12

--- list_ctrl_ips.sh
+++ list_ctrl_ips.sh
@@ -3,17 +3,17 @@ let count=`grep student_count terraform.tfvars | awk '{print $3}'`
 idx=0
 for i in `seq $count`; do
   echo -n "Student$i: "
-  terraform state show aws_eip.ctrl_eip[$idx] | grep 'public_ip ' | awk '{print $3}'
+  terraform state show aws_eip.ctrl_eip[$idx] | grep 'public_ip' | awk '{print $3}' | head -1
   ((idx++))
 done
 let count=`grep server_count terraform.tfvars | awk '{print $3}'`
 idx=0
 for i in `seq $count`; do
   echo -n "Server$i: "
-  terraform state show aws_instance.server[$idx] | grep 'private_ip ' | awk '{print $3}'
+  terraform state show aws_instance.server[$idx] | grep 'private_ip' | awk '{print $3}' | head -1
   ((idx++))
 done
 echo -n "Jumpbox: "
-terraform state show aws_instance.jump | grep 'public_ip ' | awk '{print $3}'
+terraform state show aws_instance.jump | grep 'public_ip' | awk '{print $3}' | tail -1
 echo -n "Jumpbox [private]: "
-terraform state show aws_instance.jump | grep 'private_ip ' | awk '{print $3}'
+terraform state show aws_instance.jump | grep 'private_ip' | awk '{print $3}' | head -1
